9fb3b47d6ac1a9e07bf17329d03447a31409468d
Alejandro Sanchez <alejandrosanchez9494@gmail.com>
2021-01-08 13:18:36 -0600

    Frontpage Work

--- src/components/FrontPage.js
+++ src/components/FrontPage.js
@@ -1,7 +1,40 @@
 import React from 'react';
+import axios from 'axios';
+import './FrontPage.css';
 
-const FrontPage = () => {
-    return <div> This is the front page</div>
+class FrontPage extends React.Component  {
+    constructor(){
+        super();
+        this.state = {
+            body: '',
+            img: ''
+        }
+    }
+
+addPost = () => {
+    const {body, img} = this.state;
+    axios.post('/user/post', {body, img})
+    .then( res => {
+        this.setState({
+            body: '',
+            img: ''
+        })
+    }).catch(err => console.log(err))
+}
+    render(){
+
+        let allPosts = this.state.body
+
+        return <section> 
+            <h1 style={{color: 'white'}}>This is the front page</h1>
+            <div className="post-box">
+                <textarea></textarea>
+                <button onClick={this.addPost}>Add Post</button>
+            </div>
+            <div>{allPosts}</div>
+        </section>
+
+    }
 }
 
 export default FrontPage;
