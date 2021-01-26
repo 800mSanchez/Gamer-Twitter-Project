a54fa3d358a314a42c84008cc3d63fdc38e8d249
Alejandro Sanchez <alejandrosanchez9494@gmail.com>
2021-01-26 09:32:05 -0600

    body conneceted to post works, now to display

--- src/components/FrontPage.js
+++ src/components/FrontPage.js
@@ -3,65 +3,76 @@ import axios from 'axios';
 import './FrontPage.css';
 
 class FrontPage extends React.Component  {
-    constructor(){
-        super();
+    constructor(props){
+        super(props);
         this.state = {
-            body: '',
-            img: ''
+            body: this.props.body/* ,
+            img: '' */
         }
         /* this.getPost = this.getPost.bind(this) */
     }
-componentDidMount(){
-    this.getPost()
+
+handleInputChange = e => {
+    this.setState({ body: e.target.value,
+    })
 }
 
-getPost = () => {
+handleSubmit = e => {
+    e.preventDefault();
+
+    const post = this.state;
+    
+    axios.post('/user/post')
+    .then(() => console.log('post created', post))
+    .catch(err => {
+        console.log(err);
+    });
+};
+/* componentDidMount(){
+    this.getPost() */
+
+/* getPost = () => {
     const {body,img} = this.state;
     axios.get('/user/view', {body,img}).then(res => {
         this.setState({
             body: res.data
         })
     }).catch(err => console.log(err))
-}
+} */
 
-changeHandler = e => {
-    this.setState({
-        [e.target.name]: e.target.value
-    })
-}
-
-submitHandler = e => {
-    e.preventDefault();
-    console.log(this.state);
-    axios.post('/user/post', this.state).then(res => {
-        console.log(res)
-    }).catch(err => {
-        console.log(err)
-    })
-}
-
-addPost = (post_id) => {
+ /* addPost = (post_id) => {
     const {body, img} = this.state;
     axios.post("/user/post", {post_id,body, img})
     .then( res => {
         this.setState({
             body: "",
             img: ""
-        })
+        }) 
     }).catch(err => console.log(err))
-}
+} */
     render(){
 
-        let allPosts = this.state.body
+        /* let allPosts = this.state.body.map(e => {
+            return <div>
+                <div>{e.body}</div>
+                </div>
+        }) */
 
         return <section> 
             <h1 style={{color: 'white'}}>This is the front page</h1>
-            <div className="post-box">
-                <textarea></textarea>
-                <button onClick={this.addPost}>Add Post</button>
-            </div>
-            <div>{allPosts}</div>
-        </section>
+             
+                    <div className="post-box">
+                        <input type="text" 
+                               id="post_info" 
+                               required="required" 
+                               value= {this.state.body} 
+                               onChange={this.handleInputChange}/>
+                        <button onClick={this.handleSubmit}>Add Post</button>
+                    </div>
+               {/* <div>
+                   {allPosts}
+               </div> */}
+         </section>
 
     }
 }
